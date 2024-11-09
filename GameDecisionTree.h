61bb0c28f062ee65f2a26a5556bab4849fbd53d6
Alex Huang <109326746+alexziao05@users.noreply.github.com>
2024-11-09 11:01:07 -0800

    Added BST functionality

--- GameDecisionTree.h
+++ GameDecisionTree.h
@@ -20,28 +20,59 @@ public:
     void loadStoryFromFile(const string& filename, char delimiter) {
         
         string line; 
-        ifstream myStory("story.txt"); 
+        ifstream myStory(filename); 
 
         // Check if file opened successfully 
         if (myStory.is_open()) {
 
             // Extract line by line 
             while (getline(myStory, line)) {
-                cout << line << '\n'; 
+
+                vector<string> elements; 
+                stringstream sentence(line); 
+                string item; 
+                while (getline(sentence, item, delimiter)) {
+                    elements.push_back(item); 
+                } 
+
+                // cout << elements[0] << endl; 
+                // cout << elements[1] << endl; 
+                // cout << elements[2] << endl; 
+                // cout << elements[3] << endl; 
+
+                int num = stoi(elements[0]); 
+                string desc = elements[1]; 
+                int leftNum = stoi(elements[2]);
+                int rightNum = stoi(elements[3]);
+
+                Story storyElement(desc, num, leftNum, rightNum); 
+                insert(root, storyElement); 
             }
 
             myStory.close(); 
 
         } else {
-
             cout << "Unable to open file!"; 
-            
         }
+    }
+
+    Node<T>* insert(Node<T>* root, Story storyElement) {
+        if (root  == nullptr) {
+            return new Node<T>(storyElement); 
+        }
+
+        if (storyElement.eventNumber < root->data.eventNumber) {
+            root->left = insert(root->left, storyElement); 
+        } else {
+            root->right = insert(root->right,storyElement); 
+        }
+
+        return root; 
     }
 
     // Function to start the game and traverse the tree based on user input
     void playGame() {
-
+        
     }
 };
 
