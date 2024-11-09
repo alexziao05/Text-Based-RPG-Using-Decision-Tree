2a43226055c76044c0afbc391b8d9ec9c86407f6
Alex Huang <109326746+alexziao05@users.noreply.github.com>
2024-11-09 10:00:36 -0800

    Added Read File Functionality

--- GameDecisionTree.h
+++ GameDecisionTree.h
@@ -18,7 +18,25 @@ public:
 
     // Function to load story data from a text file and build the binary tree
     void loadStoryFromFile(const string& filename, char delimiter) {
+        
+        string line; 
+        ifstream myStory("story.txt"); 
 
+        // Check if file opened successfully 
+        if (myStory.is_open()) {
+
+            // Extract line by line 
+            while (getline(myStory, line)) {
+                cout << line << '\n'; 
+            }
+
+            myStory.close(); 
+
+        } else {
+
+            cout << "Unable to open file!"; 
+            
+        }
     }
 
     // Function to start the game and traverse the tree based on user input
